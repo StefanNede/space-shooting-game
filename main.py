4aea4ca2df00d8ed31213a5ea801a9e6d7dbd653
Stefan Nedelcu <74429608+StefanNede@users.noreply.github.com>
2021-03-16 21:31:21 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-from random import randint
+import random
 import time
 
 WIDTH, HEIGHT = 400, 500
@@ -9,7 +9,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space shooters")
 FPS = 60
 VEL = 3
-BULLET_VEL = 2
+BULLET_VEL = 3
 MAX_BULLETS = 3
 ENEMY_VEL = 1
 
@@ -22,7 +22,7 @@ PLAYER = pygame.Rect(WIDTH//2-15, HEIGHT-50, 30, 30)
 
 
 class Enemy:
-    def __init__(self, xposition, yposition, width=10, height=10, velocity=ENEMY_VEL):
+    def __init__(self, xposition, yposition, velocity, width=10, height=10):
         self.width = width
         self.height = height
         self.xposition = xposition
@@ -35,11 +35,13 @@ class Enemy:
         return enemy
 
 
-def draw_window(bullets, enemy):
+def draw_window(bullets, enemies):
     WIN.fill(BLACK)
     for bullet in bullets:
         pygame.draw.rect(WIN, RED, bullet)
-    pygame.draw.rect(WIN, YELLOW, enemy)
+    for enemy in enemies:
+        # enemy[1] is pygame.Rect
+        pygame.draw.rect(WIN, YELLOW, enemy[1])
     pygame.draw.rect(WIN, WHITE, PLAYER)
     pygame.display.update()
 
@@ -51,9 +53,13 @@ def move_player(keys_pressed, player):
         player.x -= VEL
 
 
-def move_enemy(enemy): 
-    # need to check for collisions with the bullet, the player, and if it goes off the screen
-    enemy.y += 1
+def move_enemies(enemies):
+    # enemy[1] is pygame.Rect
+    # enemy[0] is object
+    for enemy in enemies:
+        enemy[1].y += enemy[0].velocity
+        if enemy[1].y > HEIGHT:
+            enemies.remove(enemy)
 
 
 def generate_bullets(bullets):
@@ -64,10 +70,13 @@ def generate_bullets(bullets):
 
 
 def main():
+    global ENEMY_VEL
     clock = pygame.time.Clock()
     run = True
     bullets = []
-    enemy = Enemy(WIDTH//2, 100).draw_enemy()
+    # this will be a 2d list that stores the enemy object at the first nested list index
+    # and the pygame rectangle of the enemy at the second index
+    enemies = []
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -77,12 +86,17 @@ def main():
                     bullet = pygame.Rect(
                         PLAYER.x + PLAYER.width//2 - 2.5, PLAYER.y, 5, 10)
                     bullets.append(bullet)
-
-        move_enemy(enemy)
+        for i in range(10 - len(enemies)):
+            ENEMY_VEL = float(random.uniform(1, 2.5))
+            xlocation = random.randint(0, WIDTH)
+            curr_enemy = [Enemy(xlocation, 0, ENEMY_VEL), Enemy(
+                xlocation, 0, ENEMY_VEL).draw_enemy()]
+            enemies.append(curr_enemy)
+        move_enemies(enemies)
         keys_pressed = pygame.key.get_pressed()
         move_player(keys_pressed, PLAYER)
         generate_bullets(bullets)
-        draw_window(bullets, enemy)
+        draw_window(bullets, enemies)
     pygame.quit()
 
 
